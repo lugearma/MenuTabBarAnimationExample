ac453d1d0c0fad3545e19fe1be15708a847266d5
Luis Arias <lugearma@gmail.com>
2018-04-27 11:27:20 -0500

    Initial Commit

---
+++ MenuTabBarExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MenuTabBarExample
+//
+//  Created by Luis Arias on 4/27/18.
+//  Copyright © 2018 Luis Arias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
